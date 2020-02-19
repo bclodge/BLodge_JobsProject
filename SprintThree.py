03b94135c16c7744fed6a1b9c60f7ada4847e192
Brandon <bclodge18@gmail.com>
2020-02-19 11:52:29 -0500

    added a test, also added a delete database to clear before running (In order to avoid unique id of link)

--- SprintThree.py
+++ SprintThree.py
@@ -1,10 +1,12 @@
 import feedparser
 import sqlite3
 
-
 parsed_feed = feedparser.parse("https://stackoverflow.com/jobs/feed")
 connection = sqlite3.connect('RSS_feed_SprintThree.sqlite')
 cursor = connection.cursor()
+
+cursor.execute('DELETE FROM RSS_pull;')  # clears the table before running, error arose on unique id of job links on
+# rerun
 
 for entries in parsed_feed['items']:
     title = entries.title
--- Tests/test_sprintOne.py
+++ Tests/test_sprintOne.py
@@ -1,6 +1,7 @@
 import SprintTwo_JobsDatabase
 import sqlite3
 # import SprintOne
+import SprintThree
 
 
 """def test_get_jobs():
@@ -34,10 +35,19 @@ def test_data_in_db():
     cursor.execute('''SELECT * FROM jobs WHERE job_id='39231b99-2aa9-4652-b6ec-2254db9f781b';   ''')
 
     rows = cursor.fetchall()
-
     for data in rows:
         assert data == "39231b99-2aa9-4652-b6ec-2254db9f781b"
 
 
+def test_SprintThree_db_exsists():
+    conn = sqlite3.connect('RSS_feed_SprintThree.sqlite')
+    cursor = conn.cursor()
+
+    cursor.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='RSS_pull' ''')
+
+    assert (cursor.fetchone()[0] == 1)
+
+
 test_table_exists()
 test_data_in_db()
+test_SprintThree_db_exsists()
