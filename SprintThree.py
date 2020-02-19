fc8a8c8c0be7aca59ffeadf81af59dd81dce9628
Brandon <bclodge18@gmail.com>
2020-02-19 11:25:30 -0500

    Sprint Three added

---
+++ SprintThree.py
@@ -0,0 +1,25 @@
+import feedparser
+import sqlite3
+
+
+parsed_feed = feedparser.parse("https://stackoverflow.com/jobs/feed")
+connection = sqlite3.connect('RSS_feed_SprintThree.sqlite')
+cursor = connection.cursor()
+
+for entries in parsed_feed['items']:
+    title = entries.title
+    link = entries.link
+    description = entries.description
+
+    print(link)
+    print(title)
+    print(description)
+
+    with connection:
+        cursor.execute('INSERT INTO main.RSS_pull (title, link, description) '
+                       'VALUES (?, ?, ?);', (entries['title'], entries['link'], entries['description']))
+
+# Commit your changes to the program
+connection.commit()
+# Close the cursor_object
+connection.close()
