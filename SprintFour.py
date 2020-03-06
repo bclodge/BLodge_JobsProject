f55bceca200f189ae7547e2da7edf1a722186f92
Brandon <bclodge18@gmail.com>
2020-03-05 22:37:54 -0500

    added in geocoder in SprintTwo_JobsDatabase.py in the table insertion in order to add the locations. Also added in the map functions to display. Need to correct the hover data issues tho

--- SprintFour.py
+++ SprintFour.py
@@ -6,50 +6,28 @@ import SprintTwo_JobsDatabase
 from geopy.extra.rate_limiter import RateLimiter
 import time
 
+import SprintOne
 
-geo_locator = Nominatim(user_agent='GoogleMaps')
+#SprintOne.main()
+
 
 conn, cursor = SprintTwo_JobsDatabase.open_db("job_database.sqlite")
 
-cursor.execute(''' CREATE TABLE IF NOT EXISTS job_locations(lat REAL, long REAL);''')
 
-cursor.execute('''SELECT company_loc FROM jobs;   ''')
+query = PD.read_sql_query('''SELECT * FROM jobs''', conn)
 
-rows = cursor.fetchall()
-counter = 0
-
-for data in rows:
-    location = geo_locator.geocode(data)
-    time.sleep(1)
-    counter = counter+1
-    try:
-        print(location.latitude, location.longitude)
-        print(counter)
-
-        cursor.execute(''' INSERT INTO job_locations(lat, long) VALUES (?,?)''',
-                       (location.latitude, location.longitude))
-    except AttributeError:
-        cursor.execute(''' INSERT INTO job_locations( lat, long) VALUES (?,?)''', (0.0, 0.0))
+dataframe_from_table = PD.DataFrame(query, columns=["Job_ID", "Job_Type", "Job_Url", "Created_at",
+                                                    "Company_posted", "Job_Location", "Job_Title",
+                                                    "Latitude", "Longitude"])
 
 
+PD.set_option('display.max_columns', None)
 
+print(dataframe_from_table)
 
-
-print(counter)
-
-# target_data = SprintOne.get_jobs()
-#
-# framed_data = PD.DataFrame(target_data, columns=["id", "title", "type", "company", "location", "latitude", "longitude"])
-#
-# PD.set_option('display.max_columns', None)
-#
-# geocode = RateLimiter(geo_locator.geocode, min_delay_seconds=1)
-#
-# # framed_data["address"] = framed_data["location"].apply(geocode)
-#
-# fig = px.scatter_mapbox(framed_data, lat="latitude", lon="longitude", hover_name="id",
-#                         hover_data=["title", "type", "company"],
-#                         color_discrete_sequence=["fuchsia"], zoom=3, height=300)
-# fig.update_layout(mapbox_style="open-street-map")
-# fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-# fig.show()
+fig = px.scatter_mapbox(dataframe_from_table, lat="Latitude", lon="Longitude", hover_name="Job_ID",
+                        hover_data=["Job_Title", "Job_Type", "Company_posted", "Created_at"],
+                        color_discrete_sequence=["fuchsia"], zoom=3, height=300)
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+fig.show()
--- SprintTwo_JobsDatabase.py
+++ SprintTwo_JobsDatabase.py
@@ -1,5 +1,9 @@
 import sqlite3
+import time
 from typing import Tuple
+from geopy import Nominatim
+
+geo_locator = Nominatim(user_agent='GoogleMaps')
 
 
 def open_db(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
@@ -15,17 +19,31 @@ def setup_db(cursor: sqlite3.Cursor):
     job_id TEXT PRIMARY KEY,\
     job_type TEXT NOT NULL,\
     job_url TEXT NOT NULL,\
-    created_at TEXT NOT NULL, company_posted TEXT NOT NULL, company_loc TEXT NOT NULL, job_title TEXT NOT NULL);''')
+    created_at TEXT NOT NULL, company_posted TEXT NOT NULL, company_loc TEXT NOT NULL, job_title TEXT NOT NULL,
+     Latitude REAL, Longitude REAL);''')
 
 
 def write_to_db(cursor: sqlite3.Cursor, data):
     cursor.execute('DELETE FROM jobs;')  # clears the table before running
     for job_posted in data:  # begin inserting values into table jobs
-        cursor.execute(''' INSERT INTO jobs(\
-            job_id, job_type, job_url, created_at, company_posted, company_loc, job_title)\
-                VALUES (?, ?, ?, ?, ?, ?, ?)''', (job_posted['id'], job_posted['type'], job_posted['url'],
-                                                  job_posted['created_at'], job_posted['company'],
-                                                  job_posted['location'], job_posted['title']))
+        temp_location_holder = geo_locator.geocode(job_posted["location"])
+        print(temp_location_holder)
+        time.sleep(2.8)
+        try:
+            cursor.execute(''' INSERT INTO jobs(\
+                job_id, job_type, job_url, created_at, company_posted, company_loc, job_title, Latitude, Longitude)\
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)''', (job_posted['id'], job_posted['type'], job_posted['url'],
+                                                            job_posted['created_at'], job_posted['company'],
+                                                            job_posted['location'], job_posted['title'],
+                                                            temp_location_holder.latitude,
+                                                            temp_location_holder.longitude))
+        except AttributeError:
+            cursor.execute(''' INSERT INTO jobs(\
+                            job_id, job_type, job_url, created_at, company_posted, company_loc, job_title, Latitude, Longitude)\
+                                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+                           (job_posted['id'], job_posted['type'], job_posted['url'],
+                            job_posted['created_at'], job_posted['company'],
+                            job_posted['location'], job_posted['title'], 0.0, 0.0))
 
 
 def close_db(connection: sqlite3.Connection):
